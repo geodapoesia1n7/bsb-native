geodapoesia1n7/bsb-native

commit 31322341ec1cb99aa6342f79121fe7093e150254

reduce ref usage

jscomp/all.depend
@@ -884,7 +884,7 @@ bsb/bsb_package_specs.cmx : ext/string_map.cmx ext/literals.cmx \
     bsb/bsb_package_specs.cmi
 bsb/bsb_parse_sources.cmx : ext/string_vec.cmx ext/string_set.cmx \
     ext/string_map.cmx ext/string_hashtbl.cmx ext/ext_string.cmx \
-    ext/ext_path.cmx ext/ext_list.cmx ext/ext_json_types.cmx ext/ext_json.cmx \
+    ext/ext_list.cmx ext/ext_json_types.cmx ext/ext_json.cmx \
     ext/ext_filename.cmx ext/ext_file_pp.cmx ext/ext_array.cmx \
     bsb/bsb_log.cmx bsb/bsb_exception.cmx bsb/bsb_dir_index.cmx \
     bsb/bsb_db.cmx bsb/bsb_build_util.cmx bsb/bsb_build_schemas.cmx \

jscomp/bin/bsb.ml
@@ -5247,8 +5247,9 @@ type t
 
 
 (**
+  [combine path1 path2]
    1. add some simplifications when concatenating
-   2. when the second one is absolute, drop the first one
+   2. when [path2] is absolute, return [path2]
 *)  
 val combine : 
   string -> 
@@ -9284,8 +9285,7 @@ type cxt = {
   t *)
 
 (** [parse_sources cxt json]
-    entry is to the [sources] in the schema
-    
+    entry is to the [sources] in the schema    
     given a root, return an object which is
     all relative paths, this function will do the IO
 *)
@@ -9346,7 +9346,7 @@ type build_generator =
     command : string}
 
 
-let is_input_or_output(xs : build_generator list) (x : string)  = 
+let is_input_or_output (xs : build_generator list) (x : string)  = 
   List.exists 
     (fun  ({input; output} : build_generator) -> 
        let it_is = (fun y -> y = x ) in
@@ -9378,10 +9378,6 @@ type t =
     globbed_dirs : string list ; 
   }
 
-let (//) = Ext_path.combine
-
-let (|?)  m (key, cb) =
-  m  |> Ext_json.test key cb 
 
 
 let warning_unused_file : _ format = 
@@ -9504,7 +9500,7 @@ let rec
   else 
     parsing_source_dir_map 
       {cxt with
-       cwd = cwd // Ext_filename.simple_convert_node_path_to_os_path dir
+       cwd = Filename.concat cwd  (Ext_filename.simple_convert_node_path_to_os_path dir)
       }
       String_map.empty
 
@@ -9521,8 +9517,6 @@ and parsing_source_dir_map
     (input : Ext_json_types.t String_map.t) : t     
   = 
   let cur_sources : Bsb_db.module_info String_map.t ref = ref String_map.empty in
-  let resources = ref [] in 
-  let public = ref Export_all in (* TODO: move to {!Bsb_default} later*)
   let cur_update_queue = ref [] in 
   let cur_globbed_dirs = ref [] in 
   let generators : build_generator list ref  = ref [] in
@@ -9635,25 +9629,34 @@ and parsing_source_dir_map
 
   end;
   let cur_sources = !cur_sources in 
-  input   
-  |?  (Bsb_build_schemas.resources ,
-       `Arr (fun s  ->
-           resources := Bsb_build_util.get_list_string s 
-         ))
-  |? (Bsb_build_schemas.public, `Str_loc (fun s loc -> 
-      if s = Bsb_build_schemas.export_all then public := Export_all else 
-      if s = Bsb_build_schemas.export_none then public := Export_none else 
+  let resources = 
+  match String_map.find_opt  Bsb_build_schemas.resources  input with 
+   | Some (Arr {content = s}) ->
+     Bsb_build_util.get_list_string s 
+   | Some config -> 
+     Bsb_exception.config_error config 
+       "expect array "  
+   | None -> [] in 
+  
+  let public = 
+    match String_map.find_opt Bsb_build_schemas.public input with 
+    | Some (Str{str = s; loc}) ->  
+      if s = Bsb_build_schemas.export_all then Export_all else 
+      if s = Bsb_build_schemas.export_none then Export_none else 
         Bsb_exception.errorf ~loc "invalid str for %s "  s 
-    ))
-  |? (Bsb_build_schemas.public, `Arr (fun s -> 
-      public := Export_set (collect_pub_modules s cur_sources)
-    ) )
-  |> ignore ;
+    | Some (Arr {content = s}) ->         
+      Export_set (collect_pub_modules s cur_sources)
+    | Some config -> 
+      Bsb_exception.config_error config "expect array or string"
+    | None ->
+      Export_all 
+  in 
+
   let cur_file = 
     {dir = dir; 
      sources = cur_sources; 
-     resources = !resources;
-     public = !public;
+     resources ;
+     public ;
      dir_index = cxt.dir_index ;
      generators ; 
     } in 
@@ -9715,10 +9718,12 @@ and parsing_source ({not_dev; dir_index ; cwd} as cxt ) (x : Ext_json_types.t )
         | None -> 
           Bsb_exception.config_error x
             (
-            "required field :" ^ Bsb_build_schemas.dir ^ " missing" )
-            
+              "required field :" ^ Bsb_build_schemas.dir ^ " missing" )
+
       in
-      parsing_source_dir_map {cxt with dir_index = current_dir_index; cwd= cwd // dir} map
+      parsing_source_dir_map 
+        {cxt with dir_index = current_dir_index; 
+                  cwd= Filename.concat cwd dir} map
   | _ -> empty 
 and  parsing_arr_sources cxt (file_groups : Ext_json_types.t array)  = 
   Array.fold_left (fun  origin x ->

jscomp/bin/bsb_helper.ml
@@ -2155,8 +2155,9 @@ type t
 
 
 (**
+  [combine path1 path2]
    1. add some simplifications when concatenating
-   2. when the second one is absolute, drop the first one
+   2. when [path2] is absolute, return [path2]
 *)  
 val combine : 
   string -> 

jscomp/bsb/bsb_parse_sources.ml
@@ -47,7 +47,7 @@ type build_generator =
     command : string}
 
 
-let is_input_or_output(xs : build_generator list) (x : string)  = 
+let is_input_or_output (xs : build_generator list) (x : string)  = 
   List.exists 
     (fun  ({input; output} : build_generator) -> 
        let it_is = (fun y -> y = x ) in
@@ -79,10 +79,6 @@ type t =
     globbed_dirs : string list ; 
   }
 
-let (//) = Ext_path.combine
-
-let (|?)  m (key, cb) =
-  m  |> Ext_json.test key cb 
 
 
 let warning_unused_file : _ format = 
@@ -205,7 +201,7 @@ let rec
   else 
     parsing_source_dir_map 
       {cxt with
-       cwd = cwd // Ext_filename.simple_convert_node_path_to_os_path dir
+       cwd = Filename.concat cwd  (Ext_filename.simple_convert_node_path_to_os_path dir)
       }
       String_map.empty
 
@@ -222,8 +218,6 @@ and parsing_source_dir_map
     (input : Ext_json_types.t String_map.t) : t     
   = 
   let cur_sources : Bsb_db.module_info String_map.t ref = ref String_map.empty in
-  let resources = ref [] in 
-  let public = ref Export_all in (* TODO: move to {!Bsb_default} later*)
   let cur_update_queue = ref [] in 
   let cur_globbed_dirs = ref [] in 
   let generators : build_generator list ref  = ref [] in
@@ -336,25 +330,34 @@ and parsing_source_dir_map
 
   end;
   let cur_sources = !cur_sources in 
-  input   
-  |?  (Bsb_build_schemas.resources ,
-       `Arr (fun s  ->
-           resources := Bsb_build_util.get_list_string s 
-         ))
-  |? (Bsb_build_schemas.public, `Str_loc (fun s loc -> 
-      if s = Bsb_build_schemas.export_all then public := Export_all else 
-      if s = Bsb_build_schemas.export_none then public := Export_none else 
+  let resources = 
+    match String_map.find_opt  Bsb_build_schemas.resources  input with 
+    | Some (Arr {content = s}) ->
+      Bsb_build_util.get_list_string s 
+    | Some config -> 
+      Bsb_exception.config_error config 
+        "expect array "  
+    | None -> [] in 
+
+  let public = 
+    match String_map.find_opt Bsb_build_schemas.public input with 
+    | Some (Str{str = s; loc}) ->  
+      if s = Bsb_build_schemas.export_all then Export_all else 
+      if s = Bsb_build_schemas.export_none then Export_none else 
         Bsb_exception.errorf ~loc "invalid str for %s "  s 
-    ))
-  |? (Bsb_build_schemas.public, `Arr (fun s -> 
-      public := Export_set (collect_pub_modules s cur_sources)
-    ) )
-  |> ignore ;
+    | Some (Arr {content = s}) ->         
+      Export_set (collect_pub_modules s cur_sources)
+    | Some config -> 
+      Bsb_exception.config_error config "expect array or string"
+    | None ->
+      Export_all 
+  in 
+
   let cur_file = 
-    {dir = dir; 
+    {dir ; 
      sources = cur_sources; 
-     resources = !resources;
-     public = !public;
+     resources ;
+     public ;
      dir_index = cxt.dir_index ;
      generators ; 
     } in 
@@ -416,10 +419,12 @@ and parsing_source ({not_dev; dir_index ; cwd} as cxt ) (x : Ext_json_types.t )
         | None -> 
           Bsb_exception.config_error x
             (
-            "required field :" ^ Bsb_build_schemas.dir ^ " missing" )
-            
+              "required field :" ^ Bsb_build_schemas.dir ^ " missing" )
+
       in
-      parsing_source_dir_map {cxt with dir_index = current_dir_index; cwd= cwd // dir} map
+      parsing_source_dir_map 
+        {cxt with dir_index = current_dir_index; 
+                  cwd= Filename.concat cwd dir} map
   | _ -> empty 
 and  parsing_arr_sources cxt (file_groups : Ext_json_types.t array)  = 
   Array.fold_left (fun  origin x ->

jscomp/ext/ext_path.mli
@@ -29,8 +29,9 @@ type t
 
 
 (**
+  [combine path1 path2]
    1. add some simplifications when concatenating
-   2. when the second one is absolute, drop the first one
+   2. when [path2] is absolute, return [path2]
 *)  
 val combine : 
   string -> 

jscomp/bin/all_ounit_tests.ml
@@ -11578,8 +11578,9 @@ type t
 
 
 (**
+  [combine path1 path2]
    1. add some simplifications when concatenating
-   2. when the second one is absolute, drop the first one
+   2. when [path2] is absolute, return [path2]
 *)  
 val combine : 
   string -> 

commit adbfbba6dd12413442b58aec3ada18910135fe61

fix a regression introduced in 31322341ec1cb99aa6342f79121fe7093e150254

jscomp/all.depend
@@ -884,7 +884,7 @@ bsb/bsb_package_specs.cmx : ext/string_map.cmx ext/literals.cmx \
     bsb/bsb_exception.cmx bsb/bsb_config.cmx bsb/bsb_build_schemas.cmx \
     bsb/bsb_package_specs.cmi
 bsb/bsb_parse_sources.cmx : ext/string_vec.cmx ext/string_set.cmx \
-    ext/string_map.cmx ext/literals.cmx ext/ext_string.cmx \
+    ext/string_map.cmx ext/literals.cmx ext/ext_string.cmx ext/ext_path.cmx \
     ext/ext_namespace.cmx ext/ext_list.cmx ext/ext_json_types.cmx \
     ext/ext_json.cmx ext/ext_filename.cmx ext/ext_file_pp.cmx \
     ext/ext_array.cmx bsb/bsb_log.cmx bsb/bsb_exception.cmx \

jscomp/bin/bsb.ml
@@ -5247,7 +5247,7 @@ type t
 
 
 (**
-  [combine path1 path2]
+   [combine path1 path2]
    1. add some simplifications when concatenating
    2. when [path2] is absolute, return [path2]
 *)  
@@ -5265,7 +5265,7 @@ val chop_extension_if_any : string -> string
 
 val chop_all_extensions_if_any : 
   string -> string 
-  
+
 (**
    {[
      get_extension "a.txt" = ".txt"
@@ -5301,6 +5301,12 @@ val rel_normalized_absolute_path : from:string -> string -> string
 val normalize_absolute_path : string -> string 
 
 val absolute_path : string Lazy.t -> string -> string
+
+(** [concat dirname filename]
+    The same as {!Filename.concat} except a tiny optimization 
+    for current directory simplification
+*)
+val concat : string -> string -> string 
 
 val check_suffix_case : 
   string -> string -> bool
@@ -5603,6 +5609,12 @@ let absolute cwd s =
   match s with 
   | File x -> File (absolute_path cwd x )
   | Dir x -> Dir (absolute_path cwd x)
+
+let concat dirname filename =
+  if filename = Filename.current_dir_name then dirname
+  else if dirname = Filename.current_dir_name then filename
+  else Filename.concat dirname filename
+  
 
 let check_suffix_case =
   Ext_string.ends_with
@@ -9665,7 +9677,7 @@ let rec
               (
                 parsing_source_dir_map
                   {cxt with 
-                   cwd = Filename.concat cxt.cwd (Ext_filename.simple_convert_node_path_to_os_path x);
+                   cwd = Ext_path.concat cxt.cwd (Ext_filename.simple_convert_node_path_to_os_path x);
                    traverse = true
                   } String_map.empty ++ origin 
               )
@@ -9720,7 +9732,7 @@ and parsing_single_source ({not_dev; dir_index ; cwd} as cxt ) (x : Ext_json_typ
     else 
       parsing_source_dir_map 
         {cxt with 
-         cwd = Filename.concat cwd (Ext_filename.simple_convert_node_path_to_os_path dir)}
+         cwd = Ext_path.concat cwd (Ext_filename.simple_convert_node_path_to_os_path dir)}
         String_map.empty  
   | Obj {map} ->
     let current_dir_index = 
@@ -9743,7 +9755,7 @@ and parsing_single_source ({not_dev; dir_index ; cwd} as cxt ) (x : Ext_json_typ
       in
       parsing_source_dir_map 
         {cxt with dir_index = current_dir_index; 
-                  cwd= Filename.concat cwd dir} map
+                  cwd= Ext_path.concat cwd dir} map
   | _ -> empty 
 and  parsing_arr_sources cxt (file_groups : Ext_json_types.t array)  = 
   Array.fold_left (fun  origin x ->

jscomp/bin/bsb_helper.ml
@@ -2155,7 +2155,7 @@ type t
 
 
 (**
-  [combine path1 path2]
+   [combine path1 path2]
    1. add some simplifications when concatenating
    2. when [path2] is absolute, return [path2]
 *)  
@@ -2173,7 +2173,7 @@ val chop_extension_if_any : string -> string
 
 val chop_all_extensions_if_any : 
   string -> string 
-  
+
 (**
    {[
      get_extension "a.txt" = ".txt"
@@ -2209,6 +2209,12 @@ val rel_normalized_absolute_path : from:string -> string -> string
 val normalize_absolute_path : string -> string 
 
 val absolute_path : string Lazy.t -> string -> string
+
+(** [concat dirname filename]
+    The same as {!Filename.concat} except a tiny optimization 
+    for current directory simplification
+*)
+val concat : string -> string -> string 
 
 val check_suffix_case : 
   string -> string -> bool
@@ -2511,6 +2517,12 @@ let absolute cwd s =
   match s with 
   | File x -> File (absolute_path cwd x )
   | Dir x -> Dir (absolute_path cwd x)
+
+let concat dirname filename =
+  if filename = Filename.current_dir_name then dirname
+  else if dirname = Filename.current_dir_name then filename
+  else Filename.concat dirname filename
+  
 
 let check_suffix_case =
   Ext_string.ends_with

jscomp/bsb/bsb_parse_sources.ml
@@ -360,7 +360,7 @@ let rec
               (
                 parsing_source_dir_map
                   {cxt with 
-                   cwd = Filename.concat cxt.cwd (Ext_filename.simple_convert_node_path_to_os_path x);
+                   cwd = Ext_path.concat cxt.cwd (Ext_filename.simple_convert_node_path_to_os_path x);
                    traverse = true
                   } String_map.empty ++ origin 
               )
@@ -415,7 +415,7 @@ and parsing_single_source ({not_dev; dir_index ; cwd} as cxt ) (x : Ext_json_typ
     else 
       parsing_source_dir_map 
         {cxt with 
-         cwd = Filename.concat cwd (Ext_filename.simple_convert_node_path_to_os_path dir)}
+         cwd = Ext_path.concat cwd (Ext_filename.simple_convert_node_path_to_os_path dir)}
         String_map.empty  
   | Obj {map} ->
     let current_dir_index = 
@@ -438,7 +438,7 @@ and parsing_single_source ({not_dev; dir_index ; cwd} as cxt ) (x : Ext_json_typ
       in
       parsing_source_dir_map 
         {cxt with dir_index = current_dir_index; 
-                  cwd= Filename.concat cwd dir} map
+                  cwd= Ext_path.concat cwd dir} map
   | _ -> empty 
 and  parsing_arr_sources cxt (file_groups : Ext_json_types.t array)  = 
   Array.fold_left (fun  origin x ->

jscomp/ext/ext_path.ml
@@ -296,5 +296,11 @@ let absolute cwd s =
   | File x -> File (absolute_path cwd x )
   | Dir x -> Dir (absolute_path cwd x)
 
+let concat dirname filename =
+  if filename = Filename.current_dir_name then dirname
+  else if dirname = Filename.current_dir_name then filename
+  else Filename.concat dirname filename
+  
+
 let check_suffix_case =
   Ext_string.ends_with

jscomp/ext/ext_path.mli
@@ -29,7 +29,7 @@ type t
 
 
 (**
-  [combine path1 path2]
+   [combine path1 path2]
    1. add some simplifications when concatenating
    2. when [path2] is absolute, return [path2]
 *)  
@@ -47,7 +47,7 @@ val chop_extension_if_any : string -> string
 
 val chop_all_extensions_if_any : 
   string -> string 
-  
+
 (**
    {[
      get_extension "a.txt" = ".txt"
@@ -83,6 +83,12 @@ val rel_normalized_absolute_path : from:string -> string -> string
 val normalize_absolute_path : string -> string 
 
 val absolute_path : string Lazy.t -> string -> string
+
+(** [concat dirname filename]
+    The same as {!Filename.concat} except a tiny optimization 
+    for current directory simplification
+*)
+val concat : string -> string -> string 
 
 val check_suffix_case : 
   string -> string -> bool

jscomp/bin/all_ounit_tests.ml
@@ -11578,7 +11578,7 @@ type t
 
 
 (**
-  [combine path1 path2]
+   [combine path1 path2]
    1. add some simplifications when concatenating
    2. when [path2] is absolute, return [path2]
 *)  
@@ -11596,7 +11596,7 @@ val chop_extension_if_any : string -> string
 
 val chop_all_extensions_if_any : 
   string -> string 
-  
+
 (**
    {[
      get_extension "a.txt" = ".txt"
@@ -11632,6 +11632,12 @@ val rel_normalized_absolute_path : from:string -> string -> string
 val normalize_absolute_path : string -> string 
 
 val absolute_path : string Lazy.t -> string -> string
+
+(** [concat dirname filename]
+    The same as {!Filename.concat} except a tiny optimization 
+    for current directory simplification
+*)
+val concat : string -> string -> string 
 
 val check_suffix_case : 
   string -> string -> bool
@@ -11934,6 +11940,12 @@ let absolute cwd s =
   match s with 
   | File x -> File (absolute_path cwd x )
   | Dir x -> Dir (absolute_path cwd x)
+
+let concat dirname filename =
+  if filename = Filename.current_dir_name then dirname
+  else if dirname = Filename.current_dir_name then filename
+  else Filename.concat dirname filename
+  
 
 let check_suffix_case =
   Ext_string.ends_with

jscomp/build_tests/namespace/input.js
@@ -7,7 +7,8 @@ child_process.execSync(`bsb -clean-world && bsb -make-world`, {cwd:__dirname, st
 
 var x = require('./src/demo.bs.js')
 var assert = require('assert')
-
+var demo_bs_js = fs.readFileSync('./src/demo.bs.js','utf8')
+assert.ok(demo_bs_js.includes('liba/src/demo.bs.js'))
 assert.equal(x.v, 3 )
 
 var merlin = fs.readFileSync(path.join(__dirname,'.merlin'), 'utf8')